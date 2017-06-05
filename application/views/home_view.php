d3e1d06d4cb801f01c80fc68b18c781669620e62
Nikhil Vengal <nikhilvengal@gmail.com>
2017-06-04 20:05:35 -0400

    Basic login page

---
+++ application/views/home_view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+ <head>
+   <title>Home</title>
+ </head>
+ <body>
+   <h1>Home</h1>
+   <h2>Welcome <?php echo $username; ?>!</h2>
+   <a href="home/logout">Logout</a>
+ </body>
+</html>
