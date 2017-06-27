e2dfa67a9bfbd093437f618d1a990aff079bed81
Nikhil Vengal <nikhilvengal@gmail.com>
2017-06-26 20:02:52 -0400

    Show manage project button only if user is project creator

--- application/views/pages/project_page_view.php
+++ application/views/pages/project_page_view.php
@@ -1,6 +1,9 @@
-
-<?php foreach($project as $row) {
-		$project_id = $row->project_id;
+<?php foreach($results as $row){
+		$user_id = $row->user_id;
+}?>
+<?php foreach($project as $row1) {
+		$project_id = $row1->project_id;
+		$created_by = $row1->project_created_by;
 }
 ?>	
 
@@ -8,9 +11,11 @@
 <br><br>
 <div class='container-fluid'>
   <div class='row-fluid'>
+  <?php if ($user_id == $created_by) : ?> 
   <div class="span4 offset4 text-center">
     <a href="<?php echo site_url()?>/Project/edit_project/<?php echo $project_id?>" class="btn btn-info" role="button">Manage Project</a>
   </div>
+  <?php endif; ?>
   </div>
 </div>
 
